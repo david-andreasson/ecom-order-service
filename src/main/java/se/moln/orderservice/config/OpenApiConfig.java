e7f8e6e36ccf8d652a8dac48d629c3a860db6e53
david-andreasson <79davand@gafe.molndal.se>
2025-09-10 13:34:40 +0200

    Updated OpenApiConfig to be able to switch between prod-server and dev-server in Swagger

--- src/main/java/se/moln/orderservice/config/OpenApiConfig.java
+++ src/main/java/se/moln/orderservice/config/OpenApiConfig.java
@@ -2,8 +2,10 @@ package se.moln.orderservice.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -13,7 +15,11 @@ public class OpenApiConfig {
         return new OpenAPI()
                 .info(new Info()
                         .title("Order Service API")
-                        .version("1.0")
-                        .description("API for managing orders in the E-commerce system."));
+                        .version("v1")
+                        .description("Order endpoints"))
+                .servers(List.of(
+                        new Server().url("http://localhost:8080").description("Local"),
+                        new Server().url("https://orderservice.drillbi.se").description("Production")
+                ));
     }
 }
